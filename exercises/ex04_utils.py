fa6a45165edd8740b0763f1dfb31ece7b8adfe9c
Camilla Fratta <camillarfratta@gmail.com>
2023-02-23 17:21:28 -0500

    changed assert to if and =0/<=0 for empty lists

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,7 +5,9 @@ __author__ = "730566852"
 
 def all(given_list: list[int], given_int: int) -> bool:
     """Check if all the items in an integer list are equal to the given integer."""
-    assert len(given_list) > 0  # check if list isn't empty
+    if len(given_list) <= 0:  # check if list isn't empty
+        return False
+    
     idx: int = 0
 
     while idx < len(given_list):  # go through each item in the list
@@ -23,7 +25,7 @@ def max(input: list[int]) -> int:
     max_int: int = input[0]  # start with assuming that the first item in the list in the max item / will compare against that
     idx: int = 0
 
-    while idx < len(input) - 1:  # check the item at each index in the list
+    while idx < len(input):  # check the item at each index in the list
         if max_int < input[idx]:  # if current max int is actually less than another item in the list
             max_int = input[idx]
         idx += 1  # if current max int is still the max int
@@ -34,12 +36,15 @@ def is_equal(first_list: list[int], second_list: list[int]) -> bool:
     """Check if two lists are identical to each other, both with items and item orders, i.e. deep equality."""
     idx: int = 0
 
-    assert len(first_list) > 0  # check for not an empty list
-    assert len(second_list) > 0  # check for not an empty list
-    assert len(first_list) == len(second_list)  # check that both lists are the same length
-
-    while idx < len(first_list) and idx < len(second_list):  # quit if you run out of items to compare
-        if first_list[idx] != second_list[idx]:  # item at one index is not the same as the item at the corresponding index in the other list
-            return False
-        idx += 1
-    return True
+    if len(first_list) < 0:  # check for not an empty list
+        return False
+    if len(second_list) < 0:  # check for not an empty list
+        return False 
+    if len(first_list) != len(second_list):  # check that both lists are the same length
+        return False
+    else:
+        while idx < len(first_list) and idx < len(second_list):  # quit if you run out of items to compare
+            if first_list[idx] != second_list[idx]:  # item at one index is not the same as the item at the corresponding index in the other list
+                return False
+            idx += 1
+        return True
