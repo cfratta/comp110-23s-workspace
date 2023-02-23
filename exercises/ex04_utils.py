3ee121c0a439916c6c67525b5b3510ec773e1ea8
Camilla Fratta <camillarfratta@gmail.com>
2023-02-23 16:58:23 -0500

    max: reorder if assert&local var declaration+while

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,9 +1,10 @@
-"""EX04 - Utils"""
+"""EX04 - Utils!"""
 
 __author__ = "730566852"
 
+
 def all(given_list: list[int], given_int: int) -> bool:
-    """Check if all the items in an integer list are equal to the given integer"""
+    """Check if all the items in an integer list are equal to the given integer."""
     assert len(given_list) > 0  # check if list isn't empty
     idx: int = 0
 
@@ -15,22 +16,22 @@ def all(given_list: list[int], given_int: int) -> bool:
 
 
 def max(input: list[int]) -> int:
-    """Find and return the max integer in a list"""
-    max_int: int = input[0]  # start with assuming that the first item in the list in the max item / will compare against that
-    idx: int = 0
-    
+    """Find and return the max integer in a list."""
     if len(input) == 0:  # empty list
         raise ValueError("max() arg is an empty List")
+    
+    max_int: int = input[0]  # start with assuming that the first item in the list in the max item / will compare against that
+    idx: int = 0
 
     while idx < len(input) - 1:  # check the item at each index in the list
-        if input[idx] < input[idx + 1]:  # if current max int is actually less than another item in the list
-            max_int = input[idx + 1]
+        if max_int < input[idx]:  # if current max int is actually less than another item in the list
+            max_int = input[idx]
         idx += 1  # if current max int is still the max int
     return max_int
 
 
 def is_equal(first_list: list[int], second_list: list[int]) -> bool:
-    """Check if two lists are identical to each other, both with items and item orders, i.e. deep equality"""
+    """Check if two lists are identical to each other, both with items and item orders, i.e. deep equality."""
     idx: int = 0
 
     assert len(first_list) > 0  # check for not an empty list
