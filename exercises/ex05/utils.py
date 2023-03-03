a20d9ab66a223118a2fd14da4feb3198b88a7cf8
Camilla Fratta <camillarfratta@gmail.com>
2023-03-03 12:11:32 -0500

    Typo: xs, not list in sub

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -5,7 +5,7 @@ __author__ = "730566852"
 
 def only_evens(xs: list[int]) -> list[int]:
     """Return even elements of an integer list."""
-    even: list[int] = []
+    even: list[int] = list()
     for elem in xs:
         if elem % 2 == 0:  # check if even number
             even.append(elem)
@@ -14,7 +14,7 @@ def only_evens(xs: list[int]) -> list[int]:
 
 def concat(list1: list[int], list2: list[int]) -> list[int]:
     """Concatinate two integer lists."""
-    concated: list[int] = []
+    concated: list[int] = list()
     for elem in list1:  # add all of the elements of the first list to new list
         concated.append(elem)
     for elem in list2:  # add all of the elements of the second list to the new list, right after first list
@@ -24,15 +24,15 @@ def concat(list1: list[int], list2: list[int]) -> list[int]:
 
 def sub(xs: list[int], start: int, stop: int) -> list[int]:
     """Return a new list based on given indexes on old list."""
-    sub_list: list[int] = []
+    sub_list: list[int] = list()
     
     if start < 0:  # start is a negative number
         start = 0
             
     if stop > len(xs):  # stop is greater than the length of the list
-        step = len(xs)
+        stop = len(xs)
     
-    if len(list) == 0 or start >= len(list) or stop <= 0:  # start is >= length of the list, or the end is at most 0, return the empty list
+    if len(xs) == 0 or start >= len(xs) or stop <= 0:  # start is >= length of the list, or the end is at most 0, return the empty list
         return sub_list
     
     for idx in range(start, stop):  # go through each element in given index range
