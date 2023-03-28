48e8a46f5b253aa1b6d85586826eecaccbc94403
Camilla Fratta <camillarfratta@gmail.com>
2023-03-28 18:18:00 -0400

    Final submitted (added rubric)

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -4,32 +4,32 @@ __author__ = "730566852"
 
 import random
 
-player: str = ""  # player name
+points: int = 5  # points used as "lives" | 0.0
+player: str = ""  # player name | 0.1
 decision: str = ""  # to play, or not to play?
 max_correct: int = 0  # player chooses difficulty
 correct: int = 0  # correct guess streak
-points: int = 5  # points used as "lives"
 play: bool = True # in order to exit game
 coin: str = "" # determine coin flip
-COIN_EMOJI: str = "\U0001FA99"
+COIN_EMOJI: str = "\U0001FA99"  # 6.1
 STARS_EMOJI: str = "\U0001F30C"
 EARNED: str = "\U0001F496"
 LOST: str = "\U0001F494"
 
 
-def main() -> None:
+def main() -> None:  # 1.0
     global play
     # Opening
-    greet()
+    greet()  # 1.1, 2.1
     while play:
-        if decision == "what are you hiding":
+        if decision == "what are you hiding":  # distracting loop
             what_are_you_hiding()
         if decision == "yes":
 
-            # Enter game loop
+            # Enter game loop | 5.0
             if max_correct == 0:
                 print(f"So, let US explain how this game works: each time you guess a coin toss correctly, you gain one life! Each time you guess a coin incorrectly, you lose a life. If you get a certain number of coin guesses correct in a row (you pick!), you win! If you run out of lives . . . well . . .")
-                correct_streak(int(input(f"Choose your difficulty! Please enter how many coins you have to guess in a row to win. (Easy: 2, Medium: 3, Hard: 4+): ")))
+                correct_streak(int(input(f"Choose your difficulty! Please enter how many coins you have to guess in a row to win. (Easy: 2, Medium: 3, Hard: 4+): ")), points)
 
             while points > 0 and max_correct > correct:
                 coin_flip()
@@ -42,28 +42,26 @@ def main() -> None:
                     print(f"Congratulations, {player}! You have won the game. Enjoy peace of mind until the next test.")
                     play = False
 
-        if decision == "no":
+        if decision == "no":  # doesn't want to play and exits game
             no()
 
 
-
-def greet() -> None:
+def greet() -> None:  #2.0
     """Greet player (ask for their name) and introduce game"""
     global player
     global decision
     print(f"Coin Flip Game!")
-    player = input(f"Please enter your name: ")
-    # COIN goes here
-    print(f"Welcome, {player}, to a simpler world. In order to survive this next test of humanity, WE put you do a random test: How many coins flips can you guess correctly in a row? {COIN_EMOJI} \n Seems cruel? Depends on how you look at it.")
+    player = input(f"Please enter your name: ")  # 2.2
+    print(f"Welcome, {player}, to a simpler world. In order to survive this next test of humanity, WE put you do a random test: How many coins flips can you guess correctly in a row? {COIN_EMOJI} \n Seems cruel? Just think about what your kind has done.")  #2.1
     decision = input(f"Want to play? Type 'yes', 'no', or 'what are you hiding': ")
 
 
-def what_are_you_hiding() -> None:
-    """Should user types 'what are you hiding' in Greet procedure, loop again"""
+def what_are_you_hiding() -> None:  # 3.1
+    """Should user type 'what are you hiding' in Greet procedure, loop again"""
     global decision
     global points
     
-    points += 1
+    points += 1  # 3.2
     print(f"O {player}, you thought that mysteries of the universe would be revealed to you? {STARS_EMOJI} Yet, WE have nothing to hide. Come, now, just play the game. \n However, you curiosity has rewarded you and you have earned a bonus life . . .")
     decision = input(f"Type 'yes' to continue or 'no' to quit: ")
 
@@ -71,17 +69,17 @@ def what_are_you_hiding() -> None:
 def no() -> None:
     """If user types 'no' in Greet procedure, exit game"""
     global play
-    print(f"So be it. But you shall come across this test again and you will not be able to say no. Until then.")
+    print(f"So be it. But you shall come across this test again and you will not be able to say no. Until then, {player}. \n Note: lives you didn't use: {points}.")  # 1.1
     play = False
 
 
-def correct_streak(user_input: int) -> int:
+def correct_streak(user_input: int, user_points: int) -> int:  # 4.1 | 4.3
     """User decides how many coins to guess in a row to win"""
     global max_correct
     global points
-    if user_input < 2:
+    if user_input < 2:  # user tries only 1 streak to win (not allowed)
         points -= 1
-        user_input = int(input(f"Oi, {player}, don't think you can outsmart US like that. We have accordingly removed one of your lives {LOST}, leaving you with a total of {points} lives. \n What difficulty do you pick, greater than 1?: "))
+        user_input = int(input(f"Oi, {player}, don't think you can outsmart US like that. We have accordingly removed one of your lives {LOST}, leaving you with a total of {points} lives. \n What difficulty do you pick, greater than 1?: "))  # 4.2
     max_correct = user_input
     return max_correct
 
@@ -89,7 +87,7 @@ def correct_streak(user_input: int) -> int:
 def coin_flip() -> str:
     """Flips coin"""
     global coin    
-    side: int = random.randint(0, 1)
+    side: int = random.randint(0, 1)  # 6.2
 
     if side == 0:
         coin = "tails"
@@ -105,11 +103,11 @@ def guess(user_input: str) -> str:
     global correct
     global coin
 
-    if user_input == coin:
+    if user_input == coin:  # correct guess
         points += 1
         correct += 1
         print(f"Correct guess, {player}! The coin toss was {coin}. Earned an extra life (total: {points})! {EARNED}")
-    elif user_input != coin:
+    elif user_input != coin:  # incorrect guess
         points -= 1
         correct = 0
         print(f"Incorrect guess, {player}. The coin toss was {coin}. Lost a life (total left: {points}) and reset count streak to zero. {LOST} ")
